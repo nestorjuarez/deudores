03bb24f6e3c0d203aa9ed31403ab6bc5287500b9
unknown <pruebanrj@hotmail.com>
2025-09-20 01:22:12 -0300

    vercel sorete

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { NextAuthOptions } from 'next-auth';
